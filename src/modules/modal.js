4985dc9c7675552366176d2f147d3f43ed97f955
AlexEgupov <alex.egupov7@gmail.com>
2022-07-12 16:41:15 +0300

    feat,refactor modal.js

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,17 +2,28 @@ export const modal = () => {
     const services = document.getElementById('services');
     const overlay = document.querySelector('.overlay');
     const servicesModal = document.querySelector('.services-modal');
+    const header = document.getElementById('header');
+    const headerModal = document.querySelector('.header-modal');
 
-    services.addEventListener('click', (e) => {
-        if (e.target.matches('.btn-sm')) {
-            overlay.style.display = 'block';
-            servicesModal.style.display = 'block';
-        }
-    });
-    servicesModal.addEventListener('click', (e) => {
-        if (e.target.matches('.services-modal__close')) {
+    const displayBlock = (btnClass, modal, block) => {
+        block.addEventListener('click', (e) => {
+            if (e.target.matches(btnClass)) {
+                overlay.style.display = 'block';
+                modal.style.display = 'block';
+            }
+        });
+    };
+
+    const displayNone = (btnClass, modal) => {
+        document.querySelector(btnClass).addEventListener('click', () => {
             overlay.style.display = 'none';
-            servicesModal.style.display = 'none';
-        }
-    });
+            modal.style.display = 'none';
+        });
+    };
+
+    displayBlock('.btn-header', headerModal, header);
+    displayNone('.header-modal__close', headerModal);
+
+    displayBlock('.btn-sm', servicesModal, services);
+    displayNone('.services-modal__close', servicesModal);
 };
